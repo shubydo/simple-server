da9b50019ae271af4b0e1a3f47117889d0f3aa17
Jack Shubatt <shubydo777@gmail.com>
2023-08-03 21:21:30 -0500

    chore: tidy modules

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,12 +3,12 @@ package server
 import "github.com/prometheus/client_golang/prometheus/promhttp"
 
 func (s *Server) routes() {
-	// Prometheus metrics
-	s.router.Handle("/metrics", promhttp.Handler())
-
 	// Main routes
 	s.router.HandleFunc("/", s.handleIndex())
 	s.router.HandleFunc("/hello", s.handleHello())
 	s.router.HandleFunc("/healthz", s.handleHealthz())
 	s.router.HandleFunc("/readyz", s.handleReadyz())
+
+	// Prometheus metrics
+	s.router.Handle("/metrics", promhttp.Handler())
 }
